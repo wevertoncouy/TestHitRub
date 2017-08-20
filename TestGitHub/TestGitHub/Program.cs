bf5d5a9f71c7486fadaf4948338c0254138687e4
wevertoncouy <weverton.wm123@hotmail.com>
2017-08-20 20:55:01 -0300

    testPC02

--- TestGitHub/TestGitHub/Program.cs
+++ TestGitHub/TestGitHub/Program.cs
@@ -15,7 +15,7 @@ namespace TestGitHub
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Teste Final");
+            Console.WriteLine("Teste Finl");
            
 
         
